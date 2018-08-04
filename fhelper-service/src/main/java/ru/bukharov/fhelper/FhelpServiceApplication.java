5aa0b7b0b29210e164169c2b3788fe25d1736443
buharov-alexander <buharovsasha@yandex.ru>
2018-08-04 11:44:02 +0700

    initial commit

---
+++ fhelper-service/src/main/java/ru/bukharov/fhelper/FhelpServiceApplication.java
@@ -0,0 +1,12 @@
+package ru.bukharov.fhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhelpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhelpServiceApplication.class, args);
+	}
+}
---
+++ fhelper-service/src/test/java/ru/bukharov/fhelper/FhelpServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.bukharov.fhelper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FhelpServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
