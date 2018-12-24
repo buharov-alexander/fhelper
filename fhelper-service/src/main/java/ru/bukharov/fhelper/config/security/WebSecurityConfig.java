6c86471f65e74eb8d5137f0b6a69a5996a083939
buharov-alexander <buharovsasha@yandex.ru>
2018-12-25 00:13:13 +0700

    backend: disable CSRF token

--- fhelper-service/src/main/java/ru/bukharov/fhelper/config/security/WebSecurityConfig.java
+++ fhelper-service/src/main/java/ru/bukharov/fhelper/config/security/WebSecurityConfig.java
@@ -16,6 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/cbr/**",
                         "/moex/**",
