164b3024ba9eba764dfdbef3eb40521088352091
buharov-alexander <buharovsasha@yandex.ru>
2019-01-10 22:47:23 +0700

    backend: add test accounts

--- fhelper-service/database/scripts/dev_state.sql
+++ fhelper-service/database/scripts/dev_state.sql
@@ -1,2 +1,7 @@
 -- Create root user (root/abcd)
-INSERT INTO "public"."users" ("username", "password", "email", "enabled") VALUES ('root', '$2a$10$yN2pc/5.2Rf5vXmgpNdFteR5zD0/CPZyMkVG0uajiTvY8a3N1fLae', '', true)
+INSERT INTO "public"."users" ("username", "password", "email", "enabled") VALUES ('root', '$2a$10$yN2pc/5.2Rf5vXmgpNdFteR5zD0/CPZyMkVG0uajiTvY8a3N1fLae', '', true);
+
+-- Create test accounts
+INSERT INTO "public"."accounts" ("name", "type", "valuta", "user_id") VALUES ('Cash rubs', 'CASH', 'RUB', 1);
+INSERT INTO "public"."accounts" ("name", "type", "valuta", "user_id") VALUES ('Alfa card rubs', 'BANK_ACCOUNT', 'RUB', 1);
+INSERT INTO "public"."accounts" ("name", "type", "valuta", "user_id") VALUES ('Alfa card usd', 'BANK_ACCOUNT', 'USD', 1);
