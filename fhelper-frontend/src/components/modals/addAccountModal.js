58b0a14580cc92964c4a35a978546eb7d643ce86
buharov-alexander <buharovsasha@yandex.ru>
2019-03-31 22:38:02 +0700

    add forms to addAccountModal

--- fhelper-frontend/src/components/modals/addAccountModal.js
+++ fhelper-frontend/src/components/modals/addAccountModal.js
@@ -1,22 +1,25 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import {
+  Modal, Button, Form, Row, Col, Dropdown, DropdownButton,
+} from 'react-bootstrap';
+import { reduxForm } from 'redux-form';
 
-import { Modal, Button } from 'react-bootstrap';
+import FormEntry from 'components/form/formEntry';
+import 'style/modals.css';
 
 class AddAccountsModal extends PureComponent {
   static propTypes = {
     closeModal: PropTypes.func.isRequired,
-  }
-
-  onSubmit = () => {
+    handleSubmit: PropTypes.func.isRequired,
   }
 
   render() {
-    const { closeModal } = this.props;
+    const { closeModal, handleSubmit } = this.props;
     return (
       <Modal
+        className="modal"
         show
-        size="lg"
         centered
       >
         <Modal.Header>
@@ -24,16 +27,49 @@ class AddAccountsModal extends PureComponent {
         </Modal.Header>
 
         <Modal.Body>
-          <p>Modal body text goes here.</p>
+          <Form className="form-add-account" onSubmit={handleSubmit}>
+            <Row className="no-margin">
+              <Col className="no-padding" md={8}>
+                <FormEntry
+                  controlId="accountName"
+                  type="text"
+                  className="grey left-part"
+                  name="accountName"
+                  placeholder="Account name"
+                />
+              </Col>
+              <Col className="no-padding">
+                <DropdownButton className="right-part" variant="secondary" title="Dropdown">
+                  <Dropdown.Item>Action</Dropdown.Item>
+                </DropdownButton>
+              </Col>
+            </Row>
+            <Row className="no-margin">
+              <Col className="no-padding" md={8}>
+                <FormEntry
+                  controlId="accountBalance"
+                  type="balance"
+                  className="grey left-part"
+                  name="accountBalance"
+                  placeholder="Balance"
+                />
+              </Col>
+              <Col className="no-padding">
+                <DropdownButton className="right-part" variant="secondary" title="Dropdown">
+                  <Dropdown.Item>Action</Dropdown.Item>
+                </DropdownButton>
+              </Col>
+            </Row>
+          </Form>
         </Modal.Body>
 
         <Modal.Footer>
-          <Button variant="secondary" onClick={closeModal}>Close</Button>
-          <Button variant="primary">Add</Button>
+          <Button className="button" variant="secondary" onClick={closeModal}>Close</Button>
+          <Button className="button" variant="primary">Add</Button>
         </Modal.Footer>
       </Modal>
     );
   }
 }
 
-export default AddAccountsModal;
+export default reduxForm({ form: 'AddAccountsModal' })(AddAccountsModal);
