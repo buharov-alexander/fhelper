fdf801e2ffd421ea8585948e6673f09806d2bfeb
Alexander Bukharov <alexander.bukharov@xored.com>
2021-09-21 22:48:23 +0700

    add fhelper-ui

---
+++ fhelper-ui/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './App.css';
+import LoginPage from './pages/login/LoginPage';
+
+function App() {
+    return <LoginPage />;
+}
+
+export default App;
---
+++ fhelper-ui/src/pages/login/LoginPage.jsx
@@ -0,0 +1,7 @@
+import {PureComponent} from 'react';
+
+export default class LoginPage extends PureComponent {
+    render() {
+        return "Login Page";
+    }
+}
