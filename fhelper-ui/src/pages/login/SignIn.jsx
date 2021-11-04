9927b4559555c1e504495de58dd0db08bbaa609e
Alexander Bukharov <alexander.bukharov@xored.com>
2021-11-04 23:45:33 +0700

    update logo

--- fhelper-ui/src/pages/login/SignIn.jsx
+++ fhelper-ui/src/pages/login/SignIn.jsx
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
+import React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 
-import {ReactComponent as GitHubSvg} from 'img/github-logo.svg'
+import {ReactComponent as GitHubSvg} from 'img/github-logo.svg';
+import {ReactComponent as ChartSvg} from 'img/chart.svg';
+
 
 export default function SignIn() {
   const handleSubmit = (event) => {
@@ -32,9 +32,7 @@ export default function SignIn() {
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
+          <ChartSvg style={{width: '50px', height: '50px'}}/>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
