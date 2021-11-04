410b7bb12f84043b7c51f6a076fbfd30ab13fe9c
Alexander Bukharov <alexander.bukharov@xored.com>
2021-11-05 00:51:19 +0700

    add App Bar

--- fhelper-ui/src/pages/home/HomePage.jsx
+++ fhelper-ui/src/pages/home/HomePage.jsx
@@ -1,10 +1,19 @@
 import React, {PureComponent} from 'react';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Toolbar from '@mui/material/Toolbar';
 
 export default class HomePage extends PureComponent {
     render() {
         return (
             <div className="b-page">
-                {'Hello'}
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                            Fhelper
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
             </div>
         );
     }
--- fhelper-ui/src/pages/login/SignIn.jsx
+++ fhelper-ui/src/pages/login/SignIn.jsx
@@ -12,77 +12,77 @@ import {ReactComponent as ChartSvg} from 'img/chart.svg';
 
 
 export default function SignIn() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        // eslint-disable-next-line no-console
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
 
-  return (
-      <Container component="main" maxWidth="xs">
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <ChartSvg style={{width: '50px', height: '50px'}}/>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+    return (
+        <Container component="main" maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
             >
-              Sign In
-            </Button>
-            <Button
-              fullWidth
-              variant="contained"
-              sx={{ mt: 0, mb: 2 }}
-              startIcon={<GitHubSvg />}
-              style={{backgroundColor: '#334a5e'}}
-            >
-              Sign In with GitHub
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link onClick={(e) => this.props.handleSwitchForm(e, 'SignUp')} variant="body2" style={{cursor: 'pointer'}}>
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-  );
+                <ChartSvg style={{width: '50px', height: '50px'}} />
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{mt: 3, mb: 2}}
+                    >
+                        Sign In
+                    </Button>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        sx={{mt: 0, mb: 2}}
+                        startIcon={<GitHubSvg />}
+                        style={{backgroundColor: '#334a5e'}}
+                    >
+                        Sign In with GitHub
+                    </Button>
+                    <Grid container>
+                        <Grid item>
+                            <Link onClick={(e) => this.props.handleSwitchForm(e, 'SignUp')} variant="body2" style={{cursor: 'pointer'}}>
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Container>
+    );
 }
